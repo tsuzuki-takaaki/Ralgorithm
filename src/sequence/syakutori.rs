07ed299a55e157b2afe45f67f3c2f215f9d55593
alexanderpierce <takaaki.110706@gmail.com>
2023-01-28 01:26:57 +0900

    add syakutori

---
+++ src/sequence/syakutori.rs
@@ -0,0 +1,24 @@
+// 尺取法
+
+// 長さnの数列aで、al + al+1 + ... + ar-1 + ar <= tを満たす整数(l, r)の組み合わせ
+fn main() {
+  input! {
+    n: usize,
+    t: usize,
+    a: [usize; n],
+  }
+
+  let mut sum = 0;
+  let mut r = 0;
+  let mut cnt = 0;
+
+  for l in 0..n {
+    while r < n && sum + a[r] <= t {
+      sum += a[r];
+      r += 1;
+    }
+    cnt += r - l;
+    if r == l { r += 1 } else { sum -= a[l] }
+  }
+  println!("{}", cnt);
+}
